f2a077f85906120294237b0b79e36673f529b945
Juan <jmfcurti@gmail.com>
2020-07-16 23:59:36 +0200

    Final details

--- googlevision.js
+++ googlevision.js
@@ -1,4 +1,5 @@
 const vision = require('@google-cloud/vision');
+const fs = require('fs');
 
 async function app(){
   const client = new vision.ImageAnnotatorClient();
@@ -10,7 +11,15 @@ async function app(){
 }
 
 function convertToHtml(text){
-  var lines = text.split("\n");
+  //preclean
+  var cleanText = text;
+  ["#t#", "#s#", "#i#", "#p#"].forEach((item, i) => {
+    cleanText = cleanText.split(item).join("\n"+item.toUpperCase());
+    cleanText = cleanText.split(item.toUpperCase()).join("\n"+item.toUpperCase());
+  });
+  cleanText = cleanText.split("\n\n").join("\n");
+
+  var lines = cleanText.split("\n");
   var resultHTML = "<html><body>"
   var imageFormat = ".jpg"
 
@@ -21,14 +30,29 @@ function convertToHtml(text){
     }else if(line.toUpperCase().includes('#S#')) {
       resultHTML = resultHTML + "<h2>"+temp+"</h2>"
     }else if(line.toUpperCase().includes('#I#')) {
-      resultHTML = resultHTML + "<img src='"+temp+imageFormat+"'/>'"
+      resultHTML = resultHTML + "<img src='"+temp+imageFormat+"'/>"
     }else if(line.toUpperCase().includes('#P#')) {
       resultHTML = resultHTML + "<p>"+temp+"</p>"
     }
   });
-
   resultHTML = resultHTML + "</body></html>"
+
+  //postclean
+  var tags = ["<h1>", "<h2>", "<p>"];
+  tags.forEach((tag, i) => {
+    resultHTML = resultHTML.split(tag+" ").join(tag);
+    var closingTag = tag.split("<").join("</");
+    resultHTML = resultHTML.split(" "+closingTag).join(closingTag);
+  });
+  resultHTML = resultHTML.split("src=' ").join("src='");
+
   console.log(resultHTML);
+  fs.writeFile("test.html", resultHTML, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("The file was generated!");
+});
 }
 
 app();
