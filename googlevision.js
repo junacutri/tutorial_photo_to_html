4184135bdac98207b51b10b9e0a93ba602f9f304
Juan <jmfcurti@gmail.com>
2020-07-16 23:19:54 +0200

    Add test image

--- googlevision.js
+++ googlevision.js
@@ -1,14 +1,34 @@
-// Imports the Google Cloud client library
 const vision = require('@google-cloud/vision');
 
 async function app(){
   const client = new vision.ImageAnnotatorClient();
-
   const fileName = 'pic2.jpg';
 
   const [result] = await client.documentTextDetection(fileName);
   const fullTextAnnotation = result.fullTextAnnotation;
-  console.log(`Full text: ${fullTextAnnotation.text}`);
+  convertToHtml(fullTextAnnotation.text);
+}
+
+function convertToHtml(text){
+  var lines = text.split("\n");
+  var resultHTML = "<html><body>"
+  var imageFormat = ".jpg"
+
+  lines.forEach((line, i) => {
+    var temp = line.substring(3);
+    if(line.toUpperCase().includes('#T#')) {
+      resultHTML = resultHTML + "<h1>"+temp+"</h1>"
+    }else if(line.toUpperCase().includes('#S#')) {
+      resultHTML = resultHTML + "<h2>"+temp+"</h2>"
+    }else if(line.toUpperCase().includes('#I#')) {
+      resultHTML = resultHTML + "<img src='"+temp+imageFormat+"'/>'"
+    }else if(line.toUpperCase().includes('#P#')) {
+      resultHTML = resultHTML + "<p>"+temp+"</p>"
+    }
+  });
+
+  resultHTML = resultHTML + "</body></html>"
+  console.log(resultHTML);
 }
 
 app();
